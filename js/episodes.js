73e36f2cd0ee46bf3442803052e550383b91e388
Het soni <hetjsoni1383@gmail.com>
2026-09-13 22:38:30 +0530

    Update Doraemon World website

--- js/episodes.js
+++ js/episodes.js
@@ -5,10 +5,10 @@ const episodes = [
     year: 2005,
     date: "2005-04-15",
     series: "new",
-    title: "The Fishing Pond in My Study Room / The Time Machine is Lost!! / Remember! The Excitement of That Day",
-    japanese: "勉強べやの釣り堀 / タイムマシンがなくなった!! / 思い出せ!あの日の感動",
-    video: "assets/episodes/episode-01.mp4",
-    cover: "assets/episodes/covers/episode-01.jpg"
-},    
+    title: "Doraemon Meets Nobita for the First Time",
+    japanese: "ドラえもんがのび太の前に初めて登場",
+    cover: "assets/episodes/covers/episode-01.jpg",
+    video: "assets/episodes/episode-01.mp4"
+} 
        
 ];
